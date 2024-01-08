a77436283612c8f28e2c9f2a91672c48a0737964
stiven <fabian.avila@iteraprocess.com>
2024-01-08 11:17:00 -0500

    dependecies

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html/drupal
-apt install wget -y
-apt install php
+yum install wget -y
+yum install php
 rm composer.phar
 wget https://getcomposer.org/composer.phar
 php composer.phar install
