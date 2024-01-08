be288d1d2d7dca520637ee2e11e406e61f3f24c5
stiven <fabian.avila@iteraprocess.com>
2024-01-08 12:33:25 -0500

    s

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -2,8 +2,8 @@
 #!/usr/bin/env bash
 
 # I have left a few things in here and will explain this further (see below)
-rsync --delete-before --verbose --archive /var/www/release/ /var/www/html/drupal/ > /var/log/deploy.log
-cd /var/www/release
+# rsync --delete-before --verbose --archive /var/www/release/ /var/www/html/drupal/ > /var/log/deploy.log
+cd /var/www/html
 yum install wget -y
 rm composer.phar
 wget https://getcomposer.org/composer.phar
