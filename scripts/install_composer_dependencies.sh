673e6d420948adf3bb9d3207bb96b8932431afbc
stiven <fabian.avila@iteraprocess.com>
2024-01-08 12:16:53 -0500

    change version to make mkdir

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "hola"
+#!/usr/bin/env bash
+
+# I have left a few things in here and will explain this further (see below)
+rsync --delete-before --verbose --archive /var/www/release/ /var/www/html/drupal/ > /var/log/deploy.log
 cd /var/www/html
 yum install wget -y
 rm composer.phar
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# I want to make sure that the directory is clean and has nothing left over from
+# previous deployments. The servers auto scale so the directory may or may not
+if [ -d /var/www/release ]; then
+    rm -rf /var/www/release
+fi
+mkdir -vp /var/www/release
+
+# exist.
 if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd24 >&2;   exit 1; fi # install apache if not already installed
