f712e435dcbc9d47f2aa3a29e7dd03edefd0c7d9
stiven <fabian.avila@iteraprocess.com>
2024-01-08 09:32:26 -0500

    update test

--- src/Hello.php
+++ src/Hello.php
@@ -1,5 +1,5 @@
 <?php
-namespace Rivsen\Demo;
+namespace stivenavilam\Demo;
 
 class Hello
 {
--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 require_once "vendor/autoload.php";
 
-$hello = new Rivsen\Demo\Hello();
+$hello = new stivenavilam\Demo\Hello();
 echo $hello->hello();
 
 echo "\n";
-$hiGirl = new Rivsen\Demo\Hello('My Goddess');
+$hiGirl = new stivenavilam\Demo\Hello('My Goddess');
 echo $hiGirl->hello();
